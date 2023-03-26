e06692a141557be7273878cf2ec29f84d4f98456
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-03-26 15:16:52 -0500

    Fixed images not loading on notion pages

--- lib/map-image-url.ts
+++ lib/map-image-url.ts
@@ -1,9 +1,12 @@
 import { Block } from 'notion-types'
 import { defaultMapImageUrl } from 'react-notion-x'
 
-import { defaultPageCover, defaultPageIcon } from './config'
+import { defaultPageCover, defaultPageIcon, rootNotionSpaceId } from './config'
 
 export const mapImageUrl = (url: string, block: Block) => {
+  if (url.includes('file.notion.so')) {
+    url = url.concat('&spaceId=', rootNotionSpaceId)
+  }
   if (url === defaultPageCover || url === defaultPageIcon) {
     return url
   }
