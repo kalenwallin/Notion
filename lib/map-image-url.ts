a0143357c15a56cc4bbd9319028cefd8351af88e
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-03-29 09:36:46 -0500

    Support for pdf url

--- lib/map-image-url.ts
+++ lib/map-image-url.ts
@@ -4,8 +4,10 @@ import { defaultMapImageUrl } from 'react-notion-x'
 import { defaultPageCover, defaultPageIcon, rootNotionSpaceId } from './config'
 
 export const mapImageUrl = (url: string, block: Block) => {
-  if (url.includes('file.notion.so')) {
-    url = url.concat('&spaceId=', rootNotionSpaceId)
+  if (url !== null) {
+    if (url.includes('file.notion.so')) {
+      url = url.concat('&spaceId=', rootNotionSpaceId)
+    }
   }
   if (url === defaultPageCover || url === defaultPageIcon) {
     return url
