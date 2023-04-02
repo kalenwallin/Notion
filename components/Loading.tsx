a50ee5f03fe9436fac9c196c4a05f71c28d1edbe
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-04-02 03:59:22 -0500

    Added custom loader

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react'
 
-import { LoadingIcon } from './LoadingIcon'
 import styles from './styles.module.css'
 
 export const Loading: React.FC = () => (
-  <div className={styles.container}>
-    <LoadingIcon />
-  </div>
+  <div className={styles.loadingContainer}></div>
 )
