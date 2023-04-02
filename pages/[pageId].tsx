1f30c3e7efe51fa3d63a7a5dd0bd96108e2e7148
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-04-02 04:18:27 -0500

    Hide v3 iframe on v3 site

--- pages/[pageId].tsx
+++ pages/[pageId].tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { GetStaticProps } from 'next'
+import { useEffect } from 'react'
 
 import { NotionPage } from '@/components/NotionPage'
 import { domain, isDev } from '@/lib/config'
@@ -50,5 +51,22 @@ export async function getStaticPaths() {
 }
 
 export default function NotionDomainDynamicPage(props) {
+  useEffect(() => {
+    // Client-side-only code
+    const url =
+      window.location != window.parent.location
+        ? document.referrer
+        : document.location.href
+    console.log(url)
+    if (url.includes('v3.kalenwallin.com')) {
+      const iframe = document.querySelector(
+        'iframe[src^="https://v3.kalenwallin.com/"]'
+      )
+      if (iframe) {
+        const parent = iframe.parentElement
+        parent.style.height = '0px'
+      }
+    }
+  })
   return <NotionPage {...props} />
 }
