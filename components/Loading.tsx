3bea5fa49ef491d2ab2f5422135b1e93dd05ba63
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-04-14 23:24:12 -0500

    Animated ball loader

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -3,5 +3,11 @@ import * as React from 'react'
 import styles from './styles.module.css'
 
 export const Loading: React.FC = () => (
-  <div className={styles.loadingContainer}></div>
+  <div className={styles.loadingContainer}>
+    <object
+      className={styles.loadingContainer}
+      type='image/svg+xml'
+      data='/ball_logo.svg'
+    ></object>
+  </div>
 )
