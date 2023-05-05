d15de829162c99fa6374a5dc823055c628259435
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-05-05 01:29:47 -0500

    Started work on a like button for pages

---
+++ components/LikeButton.tsx
@@ -0,0 +1,58 @@
+// components/LikeButton.tsx
+import React, { useEffect, useState } from 'react'
+
+import { AiFillHeart } from '@react-icons/all-files/ai/AiFillHeart'
+import { AiOutlineHeart } from '@react-icons/all-files/ai/AiOutlineHeart'
+
+import styles from './styles.module.css'
+
+type LikeButtonProps = {
+  title: string
+}
+
+const LikeButton: React.FC<LikeButtonProps> = ({ title }) => {
+  const endpoint = 'https://likes.kalenwallin.workers.dev/'
+  const [likes, setLikes] = useState(0)
+  const [likeAdded, setLikeAdded] = useState(false)
+
+  useEffect(() => {
+    loadLikes()
+  })
+
+  const loadLikes = async () => {
+    const res = await fetch(`${endpoint}?title=${title}`)
+    const data = await res.json()
+    setLikes(data.likes)
+  }
+
+  const postLike = async () => {
+    if (!likeAdded) {
+      const response = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ title })
+      })
+
+      const data = await response.json()
+      setLikes(data.likes)
+      setLikeAdded(true)
+    }
+  }
+
+  return (
+    <div
+      className={styles.likeButton}
+      style={{ '--likes': likes } as React.CSSProperties}
+    >
+      <button onClick={() => postLike()}>
+        <AiOutlineHeart />
+      </button>
+      {likes > 0 && <p>{likes}</p>}
+    </div>
+  )
+}
+
+export default LikeButton
