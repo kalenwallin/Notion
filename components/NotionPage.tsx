347da7d73867cfbe889ec5e7f3541425f8311fc0
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-06-14 17:31:30 -0500

    fix: Problem that Code block is not displayed if even one import fails.

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import dynamic from 'next/dynamic'
-import Image from "next/legacy/image"
+import Image from 'next/legacy/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
@@ -34,7 +34,7 @@ import styles from './styles.module.css'
 const Code = dynamic(() =>
   import('react-notion-x/build/third-party/code').then(async (m) => {
     // add / remove any prism syntaxes here
-    await Promise.all([
+    await Promise.allSettled([
       import('prismjs/components/prism-markup-templating.js'),
       import('prismjs/components/prism-markup.js'),
       import('prismjs/components/prism-bash.js'),
